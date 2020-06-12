c28e49ce391602f7b60ba22908dd16d3cf9e6549
flexibleOlayhemi <lyhemi@gmail.com>
2020-06-12 14:50:11 +0100

    //

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',function(){
     return view('switches');
 });
+Route::get('/test',function(){
+    return (<h1> Test page </h1>);
+});
 
 //response route on call
 Route::get('/status','iotController@index');
